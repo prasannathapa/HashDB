1ccf5b5dec37c5ab21119f63ba66b2f98e6ddbb8
Prasanna-14854 <prasanna.pt@zohocorp.com>
2024-06-22 00:30:39 +0530

    server refactor

--- src/main/java/in/prasannathapa/db/remote/Server.java
+++ src/main/java/in/prasannathapa/db/remote/Server.java
@@ -11,29 +11,29 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Server<K extends FixedRecord,V extends  FixedRecord> extends UnicastRemoteObject implements RemoteHashDB<K,V>{
+public class Server extends UnicastRemoteObject implements RemoteHashDB{
 
     protected Server() throws RemoteException {
         super();
     }
 
     @Override
-    public FixedRecord[] getAll(String dataSpace, K[] keys) throws IOException {
+    public FixedRecord[] getAll(String dataSpace, FixedRecord[] keys) throws IOException {
         return HashDB.readFrom(dataSpace).getAll(keys);
     }
 
     @Override
-    public void putAll(String dataSpace, K[] keys, V[] values) throws IOException, SizeLimitExceededException {
+    public void putAll(String dataSpace, FixedRecord[] keys, FixedRecord[] values) throws IOException, SizeLimitExceededException {
         HashDB.readFrom(dataSpace).putAll(keys, values);
     }
 
     @Override
-    public FixedRecord[] removeAndGetAll(String dataSpace, K[] keys) throws IOException {
+    public FixedRecord[] removeAndGetAll(String dataSpace, FixedRecord[] keys) throws IOException {
         return HashDB.readFrom(dataSpace).removeAll(keys);
     }
 
     @Override
-    public void removeAll(String dataSpace, K[] keys) throws IOException {
+    public void removeAll(String dataSpace, FixedRecord[] keys) throws IOException {
         HashDB.readFrom(dataSpace).removeAll(keys);
     }
 
@@ -44,11 +44,13 @@ public class Server<K extends FixedRecord,V extends  FixedRecord> extends Unicas
 
     public static void main(String[] args) {
         try {
-            // Create an instance of HelloImpl
-            Server<FixedRecord,FixedRecord> obj = new Server<>();
-            // Bind the remote object's stub in the registry
-            Registry registry = LocateRegistry.createRegistry(1099);
-            registry.bind(HashDB.class.getSimpleName(), obj);
+            int port = 1099;
+            if(args.length == 1){
+                port = Integer.parseInt(args[0]);
+            }
+            Server obj = new Server();
+            Registry registry = LocateRegistry.createRegistry(port);
+            registry.bind(HashDB.class.getName(), obj);
             System.out.println("Server Started");
         } catch (Exception e) {
             System.err.println("Server exception: " + e);
