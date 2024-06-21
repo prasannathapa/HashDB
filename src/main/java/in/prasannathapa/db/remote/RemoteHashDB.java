9769c9fc8ff7830f41381835745ad3d78e610829
Prasanna-14854 <prasanna.pt@zohocorp.com>
2024-06-22 00:30:47 +0530

    server refactor

--- src/main/java/in/prasannathapa/db/remote/RemoteHashDB.java
+++ src/main/java/in/prasannathapa/db/remote/RemoteHashDB.java
@@ -6,10 +6,10 @@ import javax.naming.SizeLimitExceededException;
 import java.io.IOException;
 import java.rmi.Remote;
 
-public interface RemoteHashDB<K extends  FixedRecord, V extends FixedRecord> extends Remote {
-    public FixedRecord[] getAll(String dataSpace, K[] keys) throws IOException;
-    public void putAll(String dataSpace, K[] keys, V[] values) throws IOException, SizeLimitExceededException;
-    public FixedRecord[] removeAndGetAll(String dataSpace, K[] keys) throws IOException;
-    public void removeAll(String dataSpace, K[] keys) throws IOException;
+public interface RemoteHashDB extends Remote {
+    public FixedRecord[] getAll(String dataSpace, FixedRecord[] keys) throws IOException;
+    public void putAll(String dataSpace, FixedRecord[] keys, FixedRecord[] values) throws IOException, SizeLimitExceededException;
+    public FixedRecord[] removeAndGetAll(String dataSpace, FixedRecord[] keys) throws IOException;
+    public void removeAll(String dataSpace, FixedRecord[] keys) throws IOException;
     public void createDB(String dataSpace, int keySize, int valueSize, int maxEntries) throws SizeLimitExceededException, IOException;
 }
